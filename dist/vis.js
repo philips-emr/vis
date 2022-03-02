9bdb7e4b496996269b5779439cae12a7709fbde9
Salustiano de Paula Franco Neto <salustiano.franco@philips.com>
2022-03-02 15:24:15 -0300

    New version flowsheet VIS

--- dist/vis.js
+++ dist/vis.js
@@ -5,7 +5,7 @@
  * A dynamic, browser-based visualization library.
  *
  * @version 4.15.4
- * @date    2022-02-17
+ * @date    2022-03-02
  *
  * @license
  * Copyright (C) 2011-2016 Almende B.V, http://almende.com
